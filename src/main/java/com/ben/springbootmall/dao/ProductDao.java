9bd665ebe420f22d127b004df36930cee98042dc
ben202065 <benwong8246@gmail.com>
2023-04-03 23:49:36 +0800

    Product Functions: Select Products

--- src/main/java/com/ben/springbootmall/dao/ProductDao.java
+++ src/main/java/com/ben/springbootmall/dao/ProductDao.java
@@ -3,8 +3,12 @@ package com.ben.springbootmall.dao;
 import com.ben.springbootmall.dto.ProductRequest;
 import com.ben.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
 
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
