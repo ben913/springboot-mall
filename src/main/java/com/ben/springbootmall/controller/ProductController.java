03c69d397d6e74737dddb552f2a29585fe776796
ben202065 <benwong8246@gmail.com>
2023-04-04 00:29:23 +0800

    Product Functions: Select pagination

--- src/main/java/com/ben/springbootmall/controller/ProductController.java
+++ src/main/java/com/ben/springbootmall/controller/ProductController.java
@@ -8,11 +8,16 @@ import com.ben.springbootmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 
+
+@Validated
 @RestController
 public class ProductController {
 
@@ -24,13 +29,17 @@ public class ProductController {
     public ResponseEntity<List<Product>> getProduct(@RequestParam(required = false) ProductCategory category,
                                                     @RequestParam(required = false) String search,
                                                     @RequestParam(defaultValue = "created_date") String orderBy,
-                                                    @RequestParam(defaultValue = "desc") String sort){
+                                                    @RequestParam(defaultValue = "desc") String sort,
+                                                    @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+                                                    @RequestParam(defaultValue = "0") @Min(0) Integer offset){
 
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
            List productList =  productService.getProducts(productQueryParams);
 
